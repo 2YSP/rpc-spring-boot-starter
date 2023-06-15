e697b6293e02cee52feceb6ee8d043bb18d1e782
ship <shi.peng@xyb2b.com>
2023-06-15 14:42:20 +0800

    GenericService

---
+++ src/main/java/cn/sp/rpc/client/generic/GenericService.java
@@ -0,0 +1,19 @@
+package cn.sp.rpc.client.generic;
+
+/**
+ * @author Ship
+ * @version 1.0.0
+ * @description:
+ * @date 2023/06/15 14:38
+ */
+public interface GenericService {
+
+    /**
+     * 泛化调用
+     * @param methodName
+     * @param parameterTypeNames
+     * @param args
+     * @return
+     */
+    Object $invoke(String methodName, String[] parameterTypeNames, Object[] args);
+}
